0fac4966ebb50a0a0426a2619d74953ed5d19ef0
yacine <issay584@email.com>
2025-05-17 21:33:57 +0100

    mission

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -5,6 +5,6 @@ int main()
     int a, b, somme;
     scanf("%d %d", &a, &b);
     somme = a + b;
-    printf("La somme est :%d %d\n", somme);
+    printf("La somme est : %d %d\n", somme);
 return 0;
 }
