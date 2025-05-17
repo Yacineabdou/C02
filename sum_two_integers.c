8800de729679a1888c7ee4cc00ed8e691db29ee5
yacine <issay584@email.com>
2025-05-17 20:36:56 +0100

    first commit

---
+++ sum_two_integers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Entrer deux nombre : \n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("La somme est :%d %d\n", a + b);
+return 0;
+}
