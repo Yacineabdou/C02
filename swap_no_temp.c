a2955b7c89f5f3b4a2d902d9f32af470613dd6ed
yacine <issay584@email.com>
2025-05-21 12:57:36 +0100

    azer

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,7 +2,13 @@
 
 int main ()
 {
-    
+    int a, b, swap;
+    scanf("%d %d", &a, &b);
+
+    swap = a;
+    a = b;
+    b = swap;
+    printf("Après échange : a = %d, b = %d\n", a, b);
 
     return 0;
 }
